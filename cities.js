c5c82be9959ac5c973fde04d3ee60fcd0e6c05ed
jared.harvick <jared.harvick@cvent.com>
2024-05-08 11:52:08 -0500

    Initial commit with data

---
+++ cities.js
@@ -0,0 +1,21 @@
+let cities = [
+    { name: "Benbrook, TX",
+      latitude: 32.6732,
+      longitude: -97.4606 },
+    { name: "Dallas, TX",
+      latitude: 32.7767,
+      longitude: -96.7970 },
+    { name: "Broken Bow, OK",
+      latitude: 34.0345,
+      longitude: -94.7405 },
+    { name: "Hanalei, HI",
+      latitude: 22.2046,
+      longitude: -159.4975 },
+    { name: "Gainesville, TX",
+      latitude: 33.6259,
+      longitude: -97.1333 },
+    { name: "Oxford, MS",
+      latitude: 34.3665,
+      longitude: -89.5192 }
+ ];
+ 
